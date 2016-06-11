7202eaa6aa46fabd4d8ea940477b4939188f5f41
Theodore <tfjtan@hotmail.com>
2016-06-11 15:45:07 -0400

    -.-

--- server.js
+++ server.js
@@ -73,31 +73,31 @@ function receivedMessage(event) {
   console.log("Received message for user %d and page %d at %d with message:",
     senderID, recipientID, timeOfMessage);
   // console.log(JSON.stringify(message));
-  var json = {recipient: {
-    id: recipientID
-  },
-  message: {
-    text: text
-  }};
-
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: 'POST',
-    json: json
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
+  // var json = {recipient: {
+  //   id: recipientID
+  // },
+  // message: {
+  //   text: text
+  // }};
+  //
+  // request({
+  //   uri: 'https://graph.facebook.com/v2.6/me/messages',
+  //   qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+  //   method: 'POST',
+  //   json: json
+  // }, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     var recipientId = body.recipient_id;
+  //     var messageId = body.message_id;
+  //
+  //     console.log("Successfully sent generic message with id %s to recipient %s",
+  //       messageId, recipientId);
+  //   } else {
+  //     console.error("Unable to send message.");
+  //     console.error(response);
+  //     console.error(error);
+  //   }
+  // });
 
   // var messageId = message.mid;
   //
