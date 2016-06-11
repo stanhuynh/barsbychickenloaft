8acbce94c1bac669385839f755cbf2f66376c610
Stanley Huynh <imstanleyhuynh@gmail.com>
2016-06-11 14:41:24 -0400

    should work

--- server.js
+++ server.js
@@ -65,40 +65,12 @@ function receivedMessage(event) {
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+  // var messageId = message.mid;
+  //
+  // // You may get a text or attachment but not both
+  // var messageText = message.text;
+  // var messageAttachments = message.attachments;
 
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 app.listen(process.env.PORT);
