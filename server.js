c51ded923a7058386b6483f183e34485e90f9feb
Stanley Huynh <imstanleyhuynh@gmail.com>
2016-06-11 14:17:01 -0400

    added bodyparser

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
 
 app.get('/', function(req, res){
   res.send('hello world');
