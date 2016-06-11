34083d3d4f37db62c060aeee61fa21901caaab3e
Stanley Huynh <imstanleyhuynh@gmail.com>
2016-06-11 13:50:49 -0400

    webhook

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('288788364');
+  res.send('hello world');
+});
+
+app.get('/webhook', function(req, res){
+  if(req.query('hub.verify_token') === 'barsbychickenloaft'){
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('error');
 });
 
 app.listen(process.env.PORT);
