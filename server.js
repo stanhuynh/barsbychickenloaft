5aa27247cd838e10917a0a89beebe0782d70584a
Theodore <tfjtan@hotmail.com>
2016-06-11 15:34:53 -0400

    put back function

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ app.post('/webhook', function (req, res) {
       var timeOfEvent = pageEntry.time;
 
       console.log(JSON.stringify(pageEntry.messaging));
+      var length = pageEntry.messaging.length;
 
-      // receivedMessage(pageEntry.messaging);
+      receivedMessage(pageEntry.messaging);
     });
 
     // Assume all went well.
@@ -59,34 +60,34 @@ function receivedMessage(event) {
   var message = event.message;
   var text = message.text;
 
-  // console.log("Received message for user %d and page %d at %d with message:",
-  //   senderID, recipientID, timeOfMessage);
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
   // console.log(JSON.stringify(message));
-  // var json = {recipient: {
-  //   id: recipientID
-  // },
-  // message: {
-  //   text: text
-  // }};
+  var json = {recipient: {
+    id: recipientID
+  },
+  message: {
+    text: text
+  }};
 
-  // request({
-  //   uri: 'https://graph.facebook.com/v2.6/me/messages',
-  //   qs: process.env.PAGE_ACCESS_TOKEN,
-  //   method: 'POST',
-  //   json: json
-  // }, function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //     var recipientId = body.recipient_id;
-  //     var messageId = body.message_id;
-  //
-  //     console.log("Successfully sent generic message with id %s to recipient %s",
-  //       messageId, recipientId);
-  //   } else {
-  //     console.error("Unable to send message.");
-  //     console.error(response);
-  //     console.error(error);
-  //   }
-  // });
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: process.env.PAGE_ACCESS_TOKEN,
+    method: 'POST',
+    json: json
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
 
   // var messageId = message.mid;
   //
