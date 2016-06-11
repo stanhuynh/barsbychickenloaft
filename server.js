d5e89691525b4b5901e7d8af8076c1cd729952cb
Theodore <tfjtan@hotmail.com>
2016-06-11 15:25:36 -0400

    send back message maybe

--- server.js
+++ server.js
@@ -73,17 +73,30 @@ function receivedMessage(event) {
   console.log("Received message for user %d and page %d at %d with message:",
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
+  var json = {recipient: {
+    id: recipientID
+  },
+  message: {
+    text: text
+  }};
 
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs:process.env.PAGE_ACCESS_TOKEN,
     method: 'POST',
-    json: {recipient: {
-      id: recipientID
-    },
-    message: {
-      text: text
-    }}
+    json: json
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
   });
 
   // var messageId = message.mid;
