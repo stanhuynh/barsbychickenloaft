5807df35c25bbe1c98b20d69ebffa283fa0e3521
Theodore <tfjtan@hotmail.com>
2016-06-11 21:49:35 -0400

    verify token

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/webhook', function(req, res){
-  if(req.query['hub.verify_token'] === 'barsbychickenloaft'){
+  if(req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
     res.send(req.query['hub.challenge']);
   }
   res.send('error');
